42410baf5fdfb87ab77d114764bd2254a17ee9a7
velasso1 <alexandr.vasin1@yandex.ru>
2025-04-04 13:36:26 +0300

    feat: add icons

--- src/app/modules/pages/general-page/components/place-block.tsx
+++ src/app/modules/pages/general-page/components/place-block.tsx
@@ -4,6 +4,12 @@ import PageTitle from "../../../ui/page-title/page-title";
 import YandexMap from "#ui/map/yandex-map.tsx";
 import Button from "#ui/button/button.tsx";
 
+import d2 from "../../../../assets/images/D2.png";
+import inst from "../../../../assets/images/mdi_instagram.png";
+import teleg from "../../../../assets/images/mingcute_telegram-fill.png";
+import facebook from "../../../../assets/images/icon-park-outline_facebook.png";
+import vk from "../../../../assets/images/basil_vk-outline.png";
+
 const PlaceBlock: FC = () => {
   return (
     <div className="place-block">
@@ -11,12 +17,29 @@ const PlaceBlock: FC = () => {
         <PageTitle pageName="контакты" />
         <YandexMap />
         <div className="place-block__action">
-          <Button
-            buttonText="открыть в картах"
-            onClickAction={() => window.open("https://yandex.ru/maps/-/CHVqFALm", "_blank")}
-          />
-
-          <div className="place-block__icons">ICONS</div>
+          <div className="place-block__button">
+            <Button
+              buttonText="открыть в картах"
+              onClickAction={() => window.open("https://yandex.ru/maps/-/CHVqFALm", "_blank")}
+            />
+          </div>
+          <div className="place-block__icons">
+            <div className="place-block__icon">
+              <img src={d2} alt="" className="place-block__icon-image" />
+            </div>
+            <div className="place-block__icon">
+              <img src={inst} alt="" className="place-block__icon-image" />
+            </div>
+            <div className="place-block__icon">
+              <img src={teleg} alt="" className="place-block__icon-image" />
+            </div>
+            <div className="place-block__icon">
+              <img src={facebook} alt="" className="place-block__icon-image" />
+            </div>
+            <div className="place-block__icon">
+              <img src={vk} alt="" className="place-block__icon-image" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
