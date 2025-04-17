cd2b04eb0b0b295fb0f4beb74eaebaf44982e1be
velasso1 <alexandr.vasin1@yandex.ru>
2025-04-17 09:18:31 +0300

    fix: video block add poster for video

--- src/app/modules/pages/general-page/components/video-block.tsx
+++ src/app/modules/pages/general-page/components/video-block.tsx
@@ -3,11 +3,21 @@ import { FC } from "react";
 import video from "../../../../assets/video/dodge-chalenger.mp4";
 import premium from "#images/premiun-caste.png";
 
+import poster from "#images/poster.png";
+
 const VideoBlock: FC = () => {
   return (
     <div className="general-page__video">
       <img className="general-page__banner" src={premium} alt="premium caste" />
-      <video className="general-page__clip" autoPlay={true} loop muted preload="auto" src={video}></video>
+      <video
+        className="general-page__clip"
+        autoPlay={true}
+        loop
+        muted
+        preload="auto"
+        poster={poster}
+        src={video}
+      ></video>
     </div>
   );
 };
