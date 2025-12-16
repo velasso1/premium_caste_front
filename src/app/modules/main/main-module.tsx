771192951bf959ca69004fa357b280209e9b3717
velasso1 <alexandr.vasin1@yandex.ru>
2025-12-16 14:45:50 +0300

    fix: add dynamic imports

--- src/app/modules/main/main-module.tsx
+++ src/app/modules/main/main-module.tsx
@@ -1,24 +1,51 @@
-import { FC } from "react";
+import { FC, lazy } from "react";
 
 import { Routes, Route } from "react-router-dom";
 
-import ServicesPage from "#pages/services-page/services-page.tsx";
-import OurWorksPage from "#pages/our-works-page/our-works-page.tsx";
-import AboutPage from "#pages/about-page/about-page.tsx";
-import BlogPage from "#pages/blog-page/blog-page.tsx";
-import ContactsPage from "#pages/contacts-page/contacts-page.tsx";
-import CartPage from "#pages/cart-page/cart-page.tsx";
-import FavoritesPage from "#pages/favorites-page/favorites-page.tsx";
-import AccountPage from "#pages/account-page/account-page.tsx";
-import NotFoundPage from "#pages/not-found-page/not-found-page.tsx";
-import CreateBlogPostPage from "#pages/create-blog-post-page/create-blog-post-page.tsx";
-import BlogPostManagingPage from "#pages/blog-post-managing-page/blog-post-managing-page.tsx";
-import EditPostPage from "#pages/edit-post-page/edit-post-page.tsx";
-import CurrentServicePage from "#pages/current-service-page/current-service-page.tsx";
-import CurrentPostPage from "#pages/current-post-page/current-post-page.tsx";
-import CreateWorkPage from "#pages/create-work-page/create-work-page.tsx";
-import CurrentWorkPage from "#pages/current-work-page/current-work-page.tsx";
-import EditWorkPage from "#pages/edit-work-page/edit-work-page.tsx";
+// import ServicesPage from "#pages/services-page/services-page.tsx";
+// import OurWorksPage from "#pages/our-works-page/our-works-page.tsx";
+// import AboutPage from "#pages/about-page/about-page.tsx";
+// import BlogPage from "#pages/blog-page/blog-page.tsx";
+// import ContactsPage from "#pages/contacts-page/contacts-page.tsx";
+// import CartPage from "#pages/cart-page/cart-page.tsx";
+// import FavoritesPage from "#pages/favorites-page/favorites-page.tsx";
+// import AccountPage from "#pages/account-page/account-page.tsx";
+// import NotFoundPage from "#pages/not-found-page/not-found-page.tsx";
+// import CreateBlogPostPage from "#pages/create-blog-post-page/create-blog-post-page.tsx";
+// import BlogPostManagingPage from "#pages/blog-post-managing-page/blog-post-managing-page.tsx";
+
+// import EditPostPage from "#pages/edit-post-page/edit-post-page.tsx";
+// import CurrentServicePage from "#pages/current-service-page/current-service-page.tsx";
+// import CurrentPostPage from "#pages/current-post-page/current-post-page.tsx";
+// import CreateWorkPage from "#pages/create-work-page/create-work-page.tsx";
+// import CurrentWorkPage from "#pages/current-work-page/current-work-page.tsx";
+// import EditWorkPage from "#pages/edit-work-page/edit-work-page.tsx";
+
+const ServicesPage = lazy(() => import("#pages/services-page/services-page.tsx"));
+const OurWorksPage = lazy(() => import("#pages/our-works-page/our-works-page.tsx"));
+
+const AboutPage = lazy(() => import("#pages/about-page/about-page.tsx"));
+
+const BlogPage = lazy(() => import("#pages/blog-page/blog-page.tsx"));
+
+const ContactsPage = lazy(() => import("#pages/contacts-page/contacts-page.tsx"));
+
+const CartPage = lazy(() => import("#pages/cart-page/cart-page.tsx"));
+
+const FavoritesPage = lazy(() => import("#pages/favorites-page/favorites-page.tsx"));
+
+const AccountPage = lazy(() => import("#pages/account-page/account-page.tsx"));
+
+const NotFoundPage = lazy(() => import("#pages/not-found-page/not-found-page.tsx"));
+
+const CreateBlogPostPage = lazy(() => import("#pages/create-blog-post-page/create-blog-post-page.tsx"));
+const BlogPostManagingPage = lazy(() => import("#pages/blog-post-managing-page/blog-post-managing-page.tsx"));
+const EditPostPage = lazy(() => import("#pages/edit-post-page/edit-post-page.tsx"));
+const CurrentServicePage = lazy(() => import("#pages/current-service-page/current-service-page.tsx"));
+const CurrentPostPage = lazy(() => import("#pages/current-post-page/current-post-page.tsx"));
+const CreateWorkPage = lazy(() => import("#pages/create-work-page/create-work-page.tsx"));
+const CurrentWorkPage = lazy(() => import("#pages/current-work-page/current-work-page.tsx"));
+const EditWorkPage = lazy(() => import("#pages/edit-work-page/edit-work-page.tsx"));
 
 import PrivateRoute from "#utils/routes/private-routes/private-route.tsx";
 import AdminRoute from "#utils/routes/private-routes/admin-route.tsx";
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// vite.config.js
+import { visualizer } from "rollup-plugin-visualizer";
+
+export default {
+  plugins: [
+    visualizer({
+      filename: "bundle-analysis.html",
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+    react(),
+  ],
+};
