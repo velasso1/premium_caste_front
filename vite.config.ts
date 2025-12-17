07b3d8ce0921ba2db8df9d416ae1ee7178530914
velasso1 <alexandr.vasin1@yandex.ru>
2025-12-17 09:44:50 +0300

    fix: config fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-// vite.config.js
-import { visualizer } from "rollup-plugin-visualizer";
-
-export default {
-  plugins: [
-    visualizer({
-      filename: "bundle-analysis.html",
-      open: true,
-      gzipSize: true,
-      brotliSize: true,
-    }),
-    react(),
-  ],
-};
+//vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
